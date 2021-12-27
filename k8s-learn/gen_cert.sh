8128dc18851d4c279ac01bbf0eb4fdf904734a66
getkub <kinbsk@gmail.com>
2021-12-27 21:36:55 +0000

    Updated k8s-non-tls

---
+++ k8s-learn/gen_cert.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+certDir="/tmp/gen_cert"
+mkdir -p ${certDir}
+ns="k8s-learn"
+cert_name="tls-secret"
+
+# TLS certificates
+openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout ${certDir}/tls.key -out ${certDir}/tls.crt -subj "/CN=nginxsvc/O=nginxsvc"
+kubectl -n $ns create secret tls ${cert_name} --key ${certDir}/tls.key --cert ${certDir}/tls.crt
+
+# # Generate the CA Key and Certificate:
+# openssl req -x509 -sha256 -newkey rsa:4096 -keyout ca.key -out ca.crt -days 356 -nodes -subj '/CN=My Cert Authority'
+
+# # Generate the Server Key, and Certificate and Sign with the CA Certificate:
+# openssl req -new -newkey rsa:4096 -keyout server.key -out server.csr -nodes -subj '/CN=local.dev'
+# openssl x509 -req -sha256 -days 365 -in server.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt
+
+# # Generate the Client Key, and Certificate and Sign with the CA Certificate:
+# openssl req -new -newkey rsa:4096 -keyout client.key -out client.csr -nodes -subj '/CN=My Client'
+# openssl x509 -req -sha256 -days 365 -in client.csr -CA ca.crt -CAkey ca.key -set_serial 02 -out client.crt
+
